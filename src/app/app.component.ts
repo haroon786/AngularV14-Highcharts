daecc5925c5df3b74c8e47cff9e5315e87e7cda3
haroon786 <haroon.ansari123@gmail.com>
2022-06-24 01:48:04 +0530

    initial angular14-highcharts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import * as Highcharts from 'highcharts';
+import { HighchartsChartModule } from 'highcharts-angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  standalone:true,
+  imports:[RouterModule,HighchartsChartModule]
+
 })
 export class AppComponent {
   title = 'AngularV14-Highcharts';
+  Highcharts = Highcharts;
+  linechart:any = {
+    series: [
+      {
+        data: [1, 2, 3]
+      }
+    ],
+    chart: {
+      type: "line"
+    },
+    title: {
+      text: "linechart"
+    }
+  };
+  barchart = {
+    series: [
+      {
+        data: [1, 2, 3]
+      }
+    ],
+    chart: {
+      type: "bar"
+    },
+    title: {
+      text: "barchart"
+    }
+  };
 }
